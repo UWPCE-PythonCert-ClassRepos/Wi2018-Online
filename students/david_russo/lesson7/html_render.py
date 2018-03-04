aa546349fa52a4582e845fca1a62737b1e5071c1
davidluizrusso <david.luiz.russo@gmail.com>
2018-03-04 13:54:11 -0800

    Added step 1

--- students/david_russo/lesson7/html_render.py
+++ students/david_russo/lesson7/html_render.py
@@ -1,7 +1,56 @@
 #!/usr/bin/env python
 
-class Element(content=None):
+class TextWrapper:
+    """
+    A simple wrapper that creates a class with a render method
+    for simple text
+    """
+    def __init__(self, text):
+        self.text = text
+
+    def render(self, file_out, current_ind=""):
+        file_out.write(current_ind)
+        file_out.write(self.text)
+
+class Element():
 	tag = "html"
 	indentation = 4 * " "
+
+	def __init__(self, content=None):
+		self.content = []
+		if content != None:
+			self.content.append(content)
+
+	def append(self, extra_content):
+		self.content.append(extra_content)
+
+	def render(self, file_out, cur_ind = ""):
+		file_out.write('<{}>\n'.format(self.tag))
+		for stuff in self.content:
+			file_out.write(cur_ind + self.indentation + stuff + '\n')
+		file_out.write('<\{}>'.format(self.tag))
+
+
+
+class Html(Element):
+	"""
+	Subclass of Element class, specifically for elements of type 'HTML'
+	"""
+	tag = "html"
+
+class Body(Element):
+	"""
+	Subclass of Element class, specifically for elements of type 'body'
+	"""
+	tag = "body"
+
+class P(Element):
+	"""
+	Subclass of Element class, specifically for element of type 'p'
+	"""
+	tag = "p"
+
+
+
 
 	
--- students/david_russo/lesson7/test_html_render.py
+++ students/david_russo/lesson7/test_html_render.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 
 import unittest
+from io import StringIO
+import html_render as hr
 
 class html_render_TestCase(unittest.TestCase):
 	
-	def test_tag_is_html(e):
+	def test_tag_is_html(self):
+		elem = hr.Element()
+		self.assertEqual(elem.tag, "html")
+	
+	def test_append(self):
+		elem = hr.Element("this")
+		elem.append("that")
+		self.assertEqual(elem.content, ["this", "that"])
+
+if __name__ == '__main__':
+    unittest.main()
+
 		
