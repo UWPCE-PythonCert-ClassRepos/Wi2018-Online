1721daa52a1dede6d0c07befe91c9ebd823e6e2d
Ken <kentodd@frontier.com>
2018-03-11 15:38:18 -0700

    updated the exchange test...__name was _name. Added the return_every_other function and trest

--- students/Kenneth_Murray/lesson3/slicing/slicing.py
+++ students/Kenneth_Murray/lesson3/slicing/slicing.py
@@ -13,17 +13,24 @@ def exchange_first_last(seq):
 		return mid
 		
 """testing the exchange function"""
-if __name__ == "_main__":
+if __name__ == "__main__":
 	string = "the string test is good"
 	list = [1,2,3,4,5,6,7]
 	assert exchange_first_last(list) == [7,2,3,4,5,6,1]
-	assert exchange_first_last(string) == "dhe string test was goot"
+	assert exchange_first_last(string) == "dhe string test is goot"
 	print ("the exchange test is complete")
 		
 			
 def return_every_other(seq, i=2):
-
-
+	"""returns every other character in a sequence"""
+	return seq[::2]
+	
+"""testing "return_every_other" function"""
+if __name__ == "__main__":
+	list = [1,2,3,4,5,6,7]
+	assert return_every_other(list) == [1, 3, 5, 7]
+	print("The return_every_other function test has competed successfully")
+	
 
 def remove_some_with_step(seq, r=-4, i=2):
 
