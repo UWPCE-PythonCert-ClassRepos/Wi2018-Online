938d71d2d71b55c44f7665bdffd97759734b71cf
Ken <kentodd@frontier.com>
2018-03-11 18:17:03 -0700

    completed the slicing fuinctions for lesson3

--- students/Kenneth_Murray/lesson3/slicing/slicing.py
+++ students/Kenneth_Murray/lesson3/slicing/slicing.py
@@ -42,22 +42,47 @@ def remove_some_with_step(seq, r=4, i=2):
 	else:
 		print("this requires a minimum of 10 characters")
 		
-"""testing return_every_other function"""
+"""testing remove_some_with_step"""
 if __name__ == "__main__":
 	list = [0,1,2,3,4,5,16,17,8,9,10,12]
 	string = "the string test is good"
-	assert return_every_other(list) == [4,16]
-	assert return_every_other(string) == 'srn eti '
-	print("The return_every_other function test has competed successfully")
-"""
+	assert remove_some_with_step(list) == [4, 16]
+	assert remove_some_with_step(string) == 'srn eti '
+	print("The remove_some_with_step test has competed successfully")
+
 
 
 def reverse_slice(seq):
+	return seq[::-1]
+	
+"""testing reverse_slice"""
+if __name__ == "__main__":
+	list = [0,1,2,3,4,5,16,17,8,9,10,12]
+	string = "the string test is good"
+	assert reverse_slice(list) == [12, 10, 9, 8, 17, 16, 5, 4, 3, 2, 1, 0]
+	assert reverse_slice(string) == 'doog si tset gnirts eht'
+	print("The reverse_slice test has competed successfully")
 
 
 
 def seq_shell_game(seq):
-
+	"""this will return the middle third first, the last third second and the first third last."""
+	"""if the number of characters is not divisible by 3, the first third will be short"""
+	length = len(seq)
+	i = round(len(seq) / 3)
+	first = seq[:i]
+	mid = seq[i:-i]
+	last = seq[length-i:]
+	return mid + last + first
+	
+"""testing seq_shell_game"""
+if __name__ == "__main__":
+	list = [0,1,2,3,4,5,16,17,8,9,10,12]
+	string = "the string test is good"
+	assert seq_shell_game(list) == [4, 5, 16, 17, 8, 9, 10, 12, 0, 1, 2, 3]
+	assert seq_shell_game(string) == 'ng test is goodthe stri'
+	print("The seq_shell_game test has competed successfully")
+	
 
 
 
