785328f3053d752a316537ac21d9f005f8bf37c0
Ken <kentodd@frontier.com>
2018-03-18 11:44:11 -0700

    getting the framework in place of the mailroom assignment

---
+++ students/Kenneth_Murray/lesson3/mailbox1/mailroom.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Contribution Mail Room"""
+contibutor_list = ["Ken Murray_1000","Ken Murray_400","Tew Tangsuk_268","Joe joe_550","Tina Tangsuk_2902","Tew Tangsuk_26568", "Nathan Merrill_2000","Joe joe_5789","Tew Tangsuk_78268"]
+def user_contibution_record:
+    """data structure that holds a list of your donors and a history of the amounts they have donated"""
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
+""""Send a Thank You”, “Create a Report” or “quit”"""
+
+"""Send a Thank You"""
+#If the user types ‘list’, show them a list of the donor names and re-prompt
+#If the user types a name not in the list, add that name to the data structure and use it.
+#If the user types a name in the list, use it.
+#Once a name has been selected, prompt for a donation amount.
+#Turn the amount into a number – it is OK at this point for the program to crash if someone types a bogus amount.
+#Once an amount has been given, add that amount to the donation history of the selected user.
+#compose an email thanking the donor for their generous donation. Print the email to the terminal and return to the original prompt.
+
+
+"""Create a Report"""
+#print a list of your donors, sorted by total historical donation amount.
+#Include Donor Name, total donated, number of donations and average donation
+#After printing this report, return to the original prompt.
+#At any point, the user should be able to quit their current task and return to the original prompt.
+#From the original prompt, the user should be able to quit the script cleanly.
+
