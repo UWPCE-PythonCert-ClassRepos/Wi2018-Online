ea56b55ff5d51a9c57f738501ae027e49178c120
Ken <kentodd@frontier.com>
2018-03-17 18:46:05 -0700

    Kenneth Murray string formatting exercise updated

--- students/Kenneth_Murray/lesson3/string_formatting/str_format_Lab_task3_rewrite.py
+++ students/Kenneth_Murray/lesson3/string_formatting/str_format_Lab_task3_rewrite.py
@@ -2,14 +2,14 @@
 """Lesson 3 task 3 string formatting re-write"""
 
 
-def number_Formatter(in_tuple) -> object:
+def number_Formatter(in_tuple):
     out_list = []
     for number in in_tuple:
-        out_list.append('${:,.2f}'.format(number))
+        out_list.append('${:,.2f}'.format(number))  # format numbers to dollar format and add to the list
     return (out_list)
 
 
 my_tuple = (1, 2, 3)
 dollars = (number_Formatter(my_tuple))
-dollar_string = ", ".join(str(x) for x in dollars)
+dollar_string = ", ".join(str(x) for x in dollars)  # create a string because it looks better when printing
 print(f'The amounts are {dollar_string}')
