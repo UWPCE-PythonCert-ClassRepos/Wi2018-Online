f6e6b6ed8639abb84c295766629e285d679d51c8
Ken <kentodd@frontier.com>
2018-03-18 16:36:15 -0700

    working on creating the mail list

--- students/Kenneth_Murray/lesson3/mailbox1/mailroom.py
+++ students/Kenneth_Murray/lesson3/mailbox1/mailroom.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 """Contribution Mail Room"""
-contibutor_list = ["Ken Murray_1000","Ken Murray_400","Tew Tangsuk_268","Joe joe_550","Tina Tangsuk_2902","Tew Tangsuk_26568", "Nathan Merrill_2000","Joe joe_5789","Tew Tangsuk_78268"]
-def user_contibution_record:
-    """data structure that holds a list of your donors and a history of the amounts they have donated"""
-    
+"""Data structure that holds a list of your donors and a history of the amounts they have donated"""
 
+
+def contributors() -> object:
+    contributor_list = [('Ken','Murray','10.00'),('Ken','Murray','4.00'),('Tew','Tangsuk','2.68'),('Joe','joe','550'),('Tina','Tangsuk','2902'),('Tew','Tangsuk','26568'),('Nathan','Merrill','2000'),('Joe','joe','5789'),('Tew','Tangsuk','78268')]
+    return contributor_list
+#def user_contibution_record(first_name, last_name, donation_amount):
+
+
+#'${:,.2f}'.format(float(tup[2]))
 
 
 
@@ -18,21 +23,31 @@ def user_contibution_record:
 
 
 if __name__ == '__main__':
+    contributor_list = contributors()
+    response = input("Hello, please select what you would like to do: (1)Thank a donor (2)Create a report (3)Quit > ")  #user will pick the activity
+    if int(response) == int(1):
+        name_list = input('Please type the name of a donor or type \"list\" to see a list of donors.  > ')
+        if not name_list == "list":
+            #do stuff here
+            print(contributor_list)
+        else:
+            names_of_donors = ()
+            for name in contibutor_list:
+                names_of_donors.__add__(name[0]+" "+name[1])
+            print(names_of_donors)
+#                    '''Send a Thank You”, “Create a Report” or “quit'''
 
+#                    '''Send a Thank You'''
+                     # If the user types ‘list’, show them a list of the donor names and re-prompt
 
-""""Send a Thank You”, “Create a Report” or “quit”"""
+                     # If the user types a name not in the list, add that name to the data structure and use it.
+                     # If the user types a name in the list, use it.
+                     # Once a name has been selected, prompt for a donation amount.
+                     # Turn the amount into a number – it is OK at this point for the program to crash if someone types a bogus amount.
+                     # Once an amount has been given, add that amount to the donation history of the selected user.
+                     # compose an email thanking the donor for their generous donation. Print the email to the terminal and return to the original prompt.
 
-"""Send a Thank You"""
-#If the user types ‘list’, show them a list of the donor names and re-prompt
-#If the user types a name not in the list, add that name to the data structure and use it.
-#If the user types a name in the list, use it.
-#Once a name has been selected, prompt for a donation amount.
-#Turn the amount into a number – it is OK at this point for the program to crash if someone types a bogus amount.
-#Once an amount has been given, add that amount to the donation history of the selected user.
-#compose an email thanking the donor for their generous donation. Print the email to the terminal and return to the original prompt.
-
-
-"""Create a Report"""
+#                     '''Create a Report'''
 #print a list of your donors, sorted by total historical donation amount.
 #Include Donor Name, total donated, number of donations and average donation
 #After printing this report, return to the original prompt.
