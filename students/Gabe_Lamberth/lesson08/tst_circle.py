f1dce731eb2c6b1663105fce783cef3a2fa484de
lambocalrissian <gncelambfam@gmail.comgit>
2018-03-18 13:50:09 -0600

    adding unit testing for circle class

---
+++ students/Gabe_Lamberth/lesson08/tst_circle.py
@@ -0,0 +1,37 @@
+import math
+from unittest import TestCase
+from circle import Circle
+
+
+class TstCircle(TestCase):
+
+    # Test Radius
+    def test_radius(self):
+        val = Circle(5)
+        self.assertEqual(val.radius, 5)
+
+    # Test Diameter
+    def test_diameter(self):
+        val = Circle(5)
+        self.assertEqual(val.diameter, 10)
+
+    # Test changing radius with evaluating diameter value
+    def test_change_radius(self):
+        c = Circle(5)
+        c.radius = 2
+        self.assertEqual(c.diameter, 4)
+
+    # Test changing diameter with evaluating radius value
+    def test_change_diameter(self):
+        c = Circle(5)
+        c.diameter = 12
+        self.assertEqual(c.radius, 6)
+
+    # Test class circle area method, value should be rounded
+    def test_area(self):
+        c = Circle(5)
+        self.assertEqual(c.area, 79)
+
+
+if __name__ == 'main':
+    TestCase()
