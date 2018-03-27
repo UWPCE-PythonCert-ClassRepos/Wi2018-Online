24ee6caf807090df504782fb8b4ef0a254abd187
Shayna Anderson-Hill <shaynaalice@gmail.com>
2018-03-27 15:59:10 -0700

    committing circle progress

--- students/shayna_andersonhill/lesson08/circle.py
+++ students/shayna_andersonhill/lesson08/circle.py
@@ -11,11 +11,26 @@ class Circle():
         self.radius = radius
         self.diameter = radius * 2
 
+    @property
+    _diameter = None
+    def diameter(self):
+        return self._diameter
+
     def area(self):
         # Return circle's area.
         return math.pi * self.radius ** 2.0
 
+    def print_circle(self):
+        print(f"My favorite circle has a radius of {self.radius} and an area of {self.area()}")
+
+    def add_circles(self, other_circle):
+        total_area = self.area() + other_circle.area()
+        return total_area
+
 # Create a Circle object.
-my_circle = Circle(2)
-print(my_circle)
-#print(my_circle.area())
+my_circle = Circle(4)
+new_circle = Circle(2)
+#print(my_circle)
+print(my_circle.area())
+my_circle.print_circle()
+print(my_circle.add_circles(new_circle))
--- students/shayna_andersonhill/lesson08/test_circle.py
+++ students/shayna_andersonhill/lesson08/test_circle.py
@@ -2,6 +2,7 @@
 from circle import Circle
 import unittest
 import pytest
+import sys
 
 #Every function is called by the test
 
@@ -11,3 +12,34 @@ def test_circle_init():
     assert my_circle.diameter == 8
 
 
+def test_circle_area():
+    my_circle = Circle(4)
+    assert my_circle.area() == 50.26548245743669
+
+
+#def test_print_circle():
+#    my_circle = Circle(4)
+#    assert print(my_circle.print_circle()) == "My favorite circle has a radius of 4 and an area of 50.26548245743669"
+
+class MyTest(unittest.TestCase):
+    def test_stdout(self):
+        my_circle = Circle(4)
+        class MyOutput(object):
+            def __init__(self):
+                self.data = []
+
+            def write(self, s):
+                self.data.append(s)
+
+            def __str__(self):
+                return "".join(self.data)
+
+        stdout_org = sys.stdout
+        my_stdout = MyOutput()
+        try:
+            sys.stdout = my_stdout
+            my_circle.print_circle()
+        finally:
+            sys.stdout = stdout_org
+
+        self.assertEquals( str(my_stdout), "My favorite circle has a radius of 4 and an area of 50.26548245743669\n") 
