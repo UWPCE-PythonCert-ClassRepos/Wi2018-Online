f2503c7528427b17c8e5ece94c44dfee58f1dcbc
Ken <kentodd@frontier.com>
2018-03-28 17:16:19 -0700

    built framework for circle class and test. Next step is to build the test. for diameter, radius, and area.

---
+++ students/Kenneth_Murray/Lesson8/circle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''The Circle Class
+inputs :
+    Diameter
+    Radius
+    print
+output:
+    diameter
+    radius
+    area
+    compare
+'''
+
+class MyCircle():
+
+        def radius(self):
+            radius = diameter / 2
+            print (diameter)
+            return diameter
+
+
+        def diameter(self):
+            diameter = radius * 2
+            print (radius)
+            return radius
+
+
+        def area(self):
+            from math import pi
+            from math import pow
+            radi = radius(self)
+            area = pi * pow(radi,2)
+            print (area)
+            return area
+
---
+++ students/Kenneth_Murray/Lesson8/test_circle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''
+Test for the Circle class
+    test_def_circle_radius
+        validates the circle attributes when a radius is used to define the circle
+        5 is used as the radius value for the test.
+    test_def_circle_diameter
+        validates the circle attributes when a diameter is used to define the circle
+        10 is used as the radius value for the test.
+
+
+'''
+
+def test_def_circle_radius(radius = 5):
+    '''
+    validates the circle attributes when a radius is used to define the circle
+    5 is used as the radius value for the test.
+    pi = import math pi module
+    Diameter should equal 10.
+    Area should equal 78.54
+    '''
+
+    return True
+
+
+def test_def_circle_diameter(diameter = 10):
+    '''
+    validates the circle attributes when a diameter is used to define the circle
+    10 is used as the diameter value for the test.
+    pi = import math pi module
+    radius should equal 5.
+    Area should equal 78.54
+    '''
+
+
+
+    return True
+
+
+
+
+
+
+
+
